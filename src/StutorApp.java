2329cfec95d9a6be29159970c6413737fa7493eb
Tan Chong Ern <ctan0119@student.monash.edu>
2021-04-09 01:01:31 +0800

    implemented basic gui (without any logic)

--- src/StutorApp.java
+++ src/StutorApp.java
@@ -2,14 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class StutorApp {
     private JFrame frame;
     private CardLayout cardLayout;
     private JPanel rootPanel, loginPage, dashboardPage;
     private JLabel activityTitle, usernameField, passwordField;
-    private JTextField usernameInput, passwordInput;
-    private JButton loginUser;
+    private JTextField usernameInput;
+    private JButton loginUserButton;
+    private JPanel registrationPage;
+    private JPanel profilePage;
+    private JButton tutorial1Button;
+    private JButton tutorial2Button;
+    private JList competenciesList;
+    private JList qualificationsList;
+    private JList initiatedBidsList;
+    private JTextField regUsernameInput;
+    private JButton registerUserButton;
+    private JLabel username;
+    private JLabel name;
+    private JLabel accountType;
+    private JPasswordField regPasswordInput;
+    private JPasswordField passwordInput;
+    private JButton tutorial3Button;
+    private JButton tutorial4Button;
+    private JButton tutorial5Button;
+    private JButton editProfileButton;
+    private JButton registerPageButton;
+    private JButton DashboardPageButton;
 
     public StutorApp() {
         frame = new JFrame("StuTor");
@@ -19,34 +41,67 @@ public class StutorApp {
 //        loginPage = new JPanel();
 //        dashboardPage = new JPanel();
 //
-//        loginUser = new JButton("log in");
-        loginUser.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {login();}
-        });
-//        loginPage.add(loginUser);
-
+//        loginUserButton = new JButton("log in");
+//        loginPage.add(loginUserButton);
 //        cardLayout = new CardLayout();
 //        rootPanel.setLayout(cardLayout);
 //        rootPanel.add(loginPage, "Login Page");
 //        rootPanel.add(dashboardPage, "Dashboard");
+//        dashboardPage.setName("dashboardPage");
+
+        loginUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {login();}
+        });
+
+        editProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) rootPanel.getLayout();
+                cl.next(rootPanel);
+            }
+        });
+
+        registerPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) rootPanel.getLayout();
+                cl.next(rootPanel);
+            }
+        });
+
+        DashboardPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) rootPanel.getLayout();
+                cl.previous(rootPanel);
+            }
+        });
+
+        registerUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) rootPanel.getLayout();
+                cl.next(rootPanel);
+            }
+        });
+
 
         frame.add(rootPanel);
         frame.setVisible(true);
     }
 
     private void login() {
+//        cl.layoutContainer(rootPanel);
+//        System.out.println(dashboardPage.getName());
+//        cl.show(rootPanel, "dashboardPage");
         CardLayout cl = (CardLayout) rootPanel.getLayout();
+        cl.next(rootPanel);
         cl.next(rootPanel);
     }
 
 
     public static void main(String[] args) {
-//        JFrame frame = new JFrame("StutorApp");
-//        frame.setContentPane(new StutorApp().rootPanel);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-//        frame.setVisible(true);
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
